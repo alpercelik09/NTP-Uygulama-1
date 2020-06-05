8ab1c4ca5467ec1b8db00246c79d60ca52002192
alpercelik <alpclk1804@gmail.com>
2020-06-05 03:21:41 +0300

    Initial commit

---
+++ src/finaluyg1/FinalUyg1.java
@@ -0,0 +1,19 @@
+
+package finaluyg1;
+
+public class FinalUyg1 {
+
+    public static void main(String[] args) 
+    {
+        System.out.println(fark(12,2));
+        System.out.println(fark(12.2,8.6));
+    }
+    static int fark (int a, int b)
+    {
+        return a-b;
+    }
+    static double fark (double a, double b)
+    {
+        return a-b;
+    }
+}
